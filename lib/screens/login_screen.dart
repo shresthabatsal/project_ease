1fb315cdd2942d201e6cd4335d0fc3a1fde1011a
batsal <shresthabatsal247@gmail.com>
2025-12-04 17:36:06 +0545

    add login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,39 +12,158 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  final emailController = TextEditingController();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login"), backgroundColor: AppColors.primary),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
+      appBar: AppBar(backgroundColor: Colors.white),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CustomTextFormField(
-                controller: emailController,
-                hintText: "Email",
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter email";
-                  }
-                  if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                    return "Please enter a valid email";
-                  }
-                  return null;
-                },
-              ),
               const SizedBox(height: 20),
-              CustomButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    print("Email is valid: ${emailController.text}");
-                  }
-                },
-                text: "Login",
+              SizedBox(
+                height: 13,
+                child: Image.asset(
+                  "assets/images/ease_logo.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+
+              const SizedBox(height: 70),
+
+              const Text(
+                "Welcome Back!",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 8),
+
+              const Text(
+                "Login to your account to continue.",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+
+              const SizedBox(height: 25),
+
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    CustomTextFormField(
+                      controller: emailController,
+                      hintText: "Email",
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Email is required.";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+
+                    CustomTextFormField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password is required.";
+                        }
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: rememberMe,
+                              activeColor: AppColors.primary,
+                              onChanged: (value) {
+                                setState(() {
+                                  rememberMe = value ?? false;
+                                });
+                              },
+                            ),
+                            const Text("Remember me"),
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: const Text(
+                            "Forgot Password?",
+                            style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    CustomButton(
+                      text: "Login",
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {}
+                      },
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Don’t have an account? ",
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: const Text(
+                            "Create one.",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    Row(
+                      children: [
+                        Expanded(child: Divider(color: Colors.grey.shade400)),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Text("or"),
+                        ),
+                        Expanded(child: Divider(color: Colors.grey.shade400)),
+                      ],
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    CustomButton(
+                      text: "Continue with Google",
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
