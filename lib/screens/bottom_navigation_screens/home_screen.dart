75930fa39abdc5623f064d9c66c2ae984ca6d248
batsal <shresthabatsal247@gmail.com>
2025-12-17 21:59:31 +0545

    add ad slider

--- lib/screens/bottom_navigation_screens/home_screen.dart
+++ lib/screens/bottom_navigation_screens/home_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:project_ease/theme/app_colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  final List<String> adImages = [
+    'assets/images/ad.png',
+    'assets/images/ad.png',
+    'assets/images/ad.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,17 +24,14 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 8),
           child: Row(
             children: [
               // Logo
-              Image.asset(
-                'assets/images/ease_logo.png',
-                height: 10,
-              ),
+              Image.asset('assets/images/ease_logo.png', height: 10),
 
               const SizedBox(width: 16),
-              
+
               // Texts
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -27,10 +39,7 @@ class HomeScreen extends StatelessWidget {
                 children: const [
                   Text(
                     'You are browsing at',
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
-                    ),
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
                   ),
                   Text(
                     'Ease',
@@ -49,7 +58,7 @@ class HomeScreen extends StatelessWidget {
         // Notification Icon
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 16),
+            padding: const EdgeInsets.only(right: 8),
             child: IconButton(
               icon: const Icon(Icons.notifications_none, color: Colors.black),
               onPressed: () {},
@@ -57,7 +66,57 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(child: Text("Home Screen")),
+      body: Column(
+        children: [
+          const SizedBox(height: 16),
+          _buildAdSlider(),
+
+          const SizedBox(height: 12),
+          _buildDotsIndicator(),
+        ],
+      ),
+    );
+  }
+
+  // Ad Slider
+  Widget _buildAdSlider() {
+    return SizedBox(
+      height: 160,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: adImages.length,
+        onPageChanged: (index) {
+          setState(() => _currentPage = index);
+        },
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(adImages[index], fit: BoxFit.cover),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  // Dots Indicator
+  Widget _buildDotsIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        adImages.length,
+        (index) => Container(
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          width: _currentPage == index ? 8 : 6,
+          height: _currentPage == index ? 8 : 6,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _currentPage == index ? AppColors.primary : Colors.grey,
+          ),
+        ),
+      ),
     );
   }
 }
