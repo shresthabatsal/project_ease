1ab412be89cfa338215d99f18260ec7677105d1c
batsal <shresthabatsal247@gmail.com>
2025-12-17 21:24:24 +0545

    add top bar in home screen

--- lib/screens/bottom_navigation_screens/home_screen.dart
+++ lib/screens/bottom_navigation_screens/home_screen.dart
@@ -1,12 +1,63 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Center(child: Text("Home Screen"),),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              // Logo
+              Image.asset(
+                'assets/images/ease_logo.png',
+                height: 10,
+              ),
+
+              const SizedBox(width: 16),
+              
+              // Texts
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text(
+                    'You are browsing at',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    'Ease',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        // Notification Icon
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: IconButton(
+              icon: const Icon(Icons.notifications_none, color: Colors.black),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+      body: const Center(child: Text("Home Screen")),
     );
   }
 }
