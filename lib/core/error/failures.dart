40c8430f0106758abade3bfb36a8cf8d84a11f92
batsal <shresthabatsal247@gmail.com>
2026-01-01 23:53:02 +0545

    add failure classes

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  const Failure(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
+
+// Local Database Failure
+class LocalDatabaseFailure extends Failure {
+  const LocalDatabaseFailure({String message = "Local database operation failed."})
+    : super(message);
+}
+
+// API Failure with status code
+class ApiFailure extends Failure {
+  final int? statusCode;
+
+  const ApiFailure({
+    required String message, 
+    this.statusCode
+    }) : super(message);
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
