4c970951a6d4990fe8e3f964a4ff92fa62937eea
batsal <shresthabatsal247@gmail.com>
2026-01-02 09:11:06 +0545

    implement auth repository in data layer

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -2,8 +2,8 @@ import 'package:project_ease/features/auth/data/models/auth_hive_model.dart';
 
 abstract interface class IAuthDatasource {
   Future<bool> registerUser(AuthHiveModel model);
-  Future<AuthHiveModel> loginUser(String email, String password);
-  Future<AuthHiveModel> getCurrentUser();
+  Future<AuthHiveModel?> loginUser(String email, String password);
+  Future<AuthHiveModel?> getCurrentUser();
   Future<bool> logoutUser();
   Future<bool> isEmailExists(String email);
 }
